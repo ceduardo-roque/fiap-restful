f3b74e0ae33df40ed29e90aa49c0c91fd4b96c4a
Carlos Eduardo Roque da Silva <ceduardo.roque@gmail.com>
2020-12-01 20:34:20 -0300

    Inclusão das assinaturas dos métodos com os contratos

--- covidwebapi/src/main/java/br/com/fiap/wsrest/covidwebapi/controller/CovidController.java
+++ covidwebapi/src/main/java/br/com/fiap/wsrest/covidwebapi/controller/CovidController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,9 +28,29 @@ public class CovidController {
 		return lista;
 	}
 	
-	@GetMapping("{id}")
-	public String buscaClienteAlunoPorID(@PathVariable Long id){
-		return "ID: " + id;
+	@GetMapping("estado/{estado}")
+	public String buscaCasosEmUmEstado(@PathVariable String estado, @RequestParam String periodoDe, @RequestParam String periodoAte){
+		return "Estado: " + estado + "; PeriodoDe: " + periodoDe + "; PeriodoAte: " + periodoAte;
+	}
+	
+	@GetMapping("estado")
+	public String buscaCasosEmDiversosEstados(@RequestParam String estados, @RequestParam String periodoDe, @RequestParam String periodoAte){
+		return "Estados: " + estados + "; PeriodoDe: " + periodoDe + "; PeriodoAte: " + periodoAte;
+	}
+	
+	@GetMapping("pais/{pais}")
+	public String buscaCasosEmUmPais(@PathVariable String pais, @RequestParam String periodoDe, @RequestParam String periodoAte){
+		return "País: " + pais + "; PeriodoDe: " + periodoDe + "; PeriodoAte: " + periodoAte;
+	}
+	
+	@GetMapping("pais")
+	public String buscaCasosEmDiversosPaises(@RequestParam String paises, @RequestParam String periodoDe, @RequestParam String periodoAte){
+		return "Paises: " + paises + "; PeriodoDe: " + periodoDe + "; PeriodoAte: " + periodoAte;
+	}
+	
+	@GetMapping("global")
+	public String buscaCasosGlobais(){
+		return "Casos Globais";
 	}
 }
 
